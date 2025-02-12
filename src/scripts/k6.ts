4fcfc3669423d9b68b2719fe487c8aa854bdff64
Daniel Laurie <dan.laurie@Dans-MacBook-Pro.local>
2025-02-12 15:35:08 +0000

    updates

--- src/scripts/k6.ts
+++ src/scripts/k6.ts
@@ -1,13 +1,17 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+const osType = __ENV.OS_TYPE || 'Darwin';
+const BASE_URL =
+  osType === 'linux' ? 'http://localhost:3000/scores' : 'http://host.docker.internal:3000/scores';
+
 export const options = {
   iterations: 10,
   VUs: 10
 };
 
 export default function () {
-  const response = http.get('http://localhost:3000/scores');
+  const response = http.get(BASE_URL);
   check(response, {
     'is status 200': (r) => r.status === 200
   });
